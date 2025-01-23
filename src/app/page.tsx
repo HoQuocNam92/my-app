91054884fcf270774713ecce6a587920215ee7b2
hoquocnam92 <hoquocnam92@gmail.com>
2025-01-23 17:27:24 +0700

    Done Domain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
-export default function Home() {
-  return (
-    <div  >
-      <h1>Hello World </h1>
-    </div>
-  );
-}
+"use client";
+import React, { useState } from 'react';
+import Board from './components/Board';
+import './gameStyle.css'
+import Helper from './components/Helper';
+const Game: React.FC = () => {
+    const [board, setBoard] = useState<(string | null)[]>(Array(9).fill(null));
+    const [xIsNext, setxIsNext] = useState(true);
+    const winner = Helper(board)
+    const handleClick = (index: number) => {
+        const boardCopy = [...board];
+        if (winner || boardCopy[index]) {
+            return;
+        }
+        boardCopy[index] = xIsNext ? 'X' : 'O';
+        setBoard(boardCopy);
+        setxIsNext(!xIsNext);
+    }
+    const handleResetGame = () => {
+        setBoard(Array(9).fill(null));
+        setxIsNext(true);
+    }
+    return (
+        <div >
+            <Board cells={board} onClick={handleClick} />
+            <button onClick={handleResetGame}>Reset game</button>
+        </div>
+    );
+};
+
+export default Game;
